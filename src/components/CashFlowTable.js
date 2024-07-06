ed95de0b5974761b4c4598092958b63d8f454374
Will Asbury <asburywg@gmail.com>
2024-07-06 16:37:40 -0400

    expander

--- src/components/CashFlowTable.js
+++ src/components/CashFlowTable.js
@@ -14,54 +14,75 @@ const formatDateHeader = (dateString) => {
 };
 
 const data = [
-    {
-      "category": "Income",
-      "2024-03": 11118.779999999999,
-      "2024-04": 7590.71,
-      "2024-05": 7868.7699999999995,
-      "subRows": [
-        {
-          "category": "Interest Income",
-          "2024-03": 131.23,
-          "2024-04": 164.51999999999998,
-          "2024-05": 179.57
-        },
-        {
-          "category": "Misc Income",
-          "2024-05": 488.0
-        }
-      ]
-    },
-    {
-      "category": "Expenses",
-      "2024-03": -4638.75,
-      "2024-04": -6863.8,
-      "2024-05": -7591.659999999999,
-      "subRows": [
-        {
-          "category": "Altis",
-          "2024-03": -3158.41,
-          "2024-04": -3151.2599999999998,
-          "2024-05": -3160.3599999999997,
-          "subRows": [
-            {
-              "category": "Altis Rent",
-
-              "2024-03": -3094.25,
-              "2024-04": -3088.14,
-              "2024-05": -3089.95
-
-            }
-          ]
-        }]
-    },
-    {
-      "category": "Net Income",
-      "2024-03": 123.75,
-      "2024-04": 863.8,
-      "2024-05": 91.659999999999
-    }
-  ]
+  {
+    "category": "Income",
+    "2024-03": 11118.779999999999,
+    "2024-04": 7590.71,
+    "2024-05": 7868.7699999999995,
+    "subRows": [
+      {
+        "category": "Interest Income",
+        "2024-03": 131.23,
+        "2024-04": 164.51999999999998,
+        "2024-05": 179.57
+      },
+      {
+        "category": "Misc Income",
+        "2024-05": 488.0
+      }
+    ]
+  },
+  {
+    "category": "Expenses",
+    "2024-03": -4638.75,
+    "2024-04": -6863.8,
+    "2024-05": -7591.659999999999,
+    "subRows": [
+      {
+        "category": "Living",
+        "2024-03": -3158.41,
+        "2024-04": -3151.2599999999998,
+        "2024-05": -3160.3599999999997,
+        "subRows": [
+          {
+            "category": "Altis",
+            "2024-03": -3094.25,
+            "2024-04": -3088.14,
+            "2024-05": -3089.95,
+            "subRows": [
+              {
+                "category": "Altis Rent",
+                "2024-03": -3094.25,
+                "2024-04": -3088.14,
+                "2024-05": -3089.95,
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "category": "Leisure",
+        "2024-03": -3158.41,
+        "2024-04": -3151.2599999999998,
+        "2024-05": -3160.3599999999997,
+        "subRows": [
+          {
+            "category": "Altis Rent",
+            "2024-03": -3094.25,
+            "2024-04": -3088.14,
+            "2024-05": -3089.95,
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "category": "Net Income",
+    "2024-03": 123.75,
+    "2024-04": 863.8,
+    "2024-05": 91.659999999999
+  }
+]
 
 const headers = [
   {
@@ -142,31 +163,31 @@ const CashFlowTable = () => {
     enableFilters: false,
     enableColumnActions: false,
     enablePagination: false,
-    initialState: { 
+    initialState: {
       density: 'compact'
     },
     displayColumnDefOptions: {
       'mrt-row-expand': {
-        Header: () => (  
+        Header: () => (
           <MRT_ExpandAllButton table={table} />
         ),
         muiTableBodyCellProps: ({ row }) => ({
           sx: () => ({
-            maxWidth: '3rem',
-            width: '3rem',
-            backgroundColor: ( ['Net Income', 'Income', 'Expenses'].includes(row.original.category)) ? '#F5F5F5': ''
+            maxWidth: '4rem',
+            width: '4rem',
+            backgroundColor: (['Net Income', 'Income', 'Expenses'].includes(row.original.category)) ? '#F5F5F5' : ''
           }),
         }),
-        size: 30,
-        minSize: 30,
+        size: 40,
+        minSize: 40,
       },
     },
     muiTableBodyCellProps: ({ row }) => ({
       sx: {
-        backgroundColor: ( ['Net Income', 'Income', 'Expenses'].includes(row.original.category)) ? '#F5F5F5': ''
+        backgroundColor: (['Net Income', 'Income', 'Expenses'].includes(row.original.category)) ? '#F5F5F5' : ''
       },
     }),
-    
+
   });
 
   return <MaterialReactTable table={table} />;
