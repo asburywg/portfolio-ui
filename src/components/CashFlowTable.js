be8d2c4e231fb7ae2cc82c52cc4aef8346f44403
Will Asbury <asburywg@gmail.com>
2024-07-06 18:19:21 -0400

    format

--- src/components/CashFlowTable.js
+++ src/components/CashFlowTable.js
@@ -11,8 +11,14 @@ const formatDateHeader = (dateString) => {
   const date = new Date(dateString + '-01'); // Add day part to create a valid date
   return date.toLocaleString('default', { month: 'short', year: 'numeric' });
 };
+const res = {
+  'columns': [{
+    'accessorKey': 'category', 'header': 'Category'
+  }, { 'accessorKey': '2024-03', 'header': '2024-03' }, { 'accessorKey': '2024-02', 'header': '2024-02' }, { 'accessorKey': '2024-01', 'header': '2024-01' }], 'rows': [{ 'category': 'Income', '2024-03': 11118.779999999999, '2024-02': 7327.7300000000005, '2024-01': 9100.910000000002, 'subRows': [{ 'category': 'Interest Income', '2024-03': 131.23, '2024-02': 126.53, '2024-01': 111.85 }, { 'category': 'Misc Income', '2024-01': 205.0 }, { 'category': 'Paycheck', '2024-03': 10987.55, '2024-02': 7201.200000000001, '2024-01': 8784.060000000001 }] }, { 'category': 'Expenses', '2024-03': -4638.75, '2024-02': -5012.679999999999, '2024-01': -5157.5, 'subRows': [{ 'category': 'Living', '2024-03': -3926.29, '2024-02': -4305.23, '2024-01': -4037.14, 'subRows': [{ 'category': 'Altis Rent', '2024-03': -3094.25, '2024-02': -3081.66, '2024-01': -3076.35 }, { 'category': 'Altis Utilities & Cost', '2024-03': -64.16, '2024-02': -58.74, '2024-01': -71.33 }, { 'category': 'Bills & Subscriptions', '2024-03': -307.47999999999996, '2024-02': -327.87000000000006, '2024-01': -303.9 }, { 'category': 'Gas & Fuel', '2024-02': -41.97 }, { 'category': 'Groceries', '2024-03': -460.40000000000003, '2024-02': -333.75999999999993, '2024-01': -563.56 }, { 'category': 'Misc Expenses', '2024-02': -461.23, '2024-01': -22.0 }] }, { 'category': 'Leisure', '2024-03': -712.46, '2024-02': -707.4499999999999, '2024-01': -1120.36, 'subRows': [{ 'category': 'Clothing', '2024-03': -34.72, '2024-02': 122.27000000000001, '2024-01': -517.02 }, { 'category': 'Convenience', '2024-03': -139.65, '2024-02': -150.33999999999997, '2024-01': -153.74 }, { 'category': 'Food & Dining', '2024-03': -254.65999999999997, '2024-02': -242.42999999999995, '2024-01': -192.76 }, { 'category': 'Shopping', '2024-03': -283.43, '2024-02': -379.8, '2024-01': -230.92000000000002 }, { 'category': 'Travel', '2024-02': -57.15, '2024-01': -25.92 }] }, { 'category': 'Uncategorized', '2024-03': 0, '2024-02': 0, '2024-01': 0, 'subRows': [] }] }, { 'category': 'Net Profit', '2024-03': 6480.029999999999, '2024-02': 2315.050000000001, '2024-01': 3943.4100000000017, 'subRows': [] }]
+}
 
-const data = [
+
+const rowdata = [
   {
     "category": "Income",
     "2024-03": 11118.779999999999,
@@ -84,7 +90,7 @@ const data = [
   }
 ]
 
-const headers = [
+const headerdata = [
   {
     "accessorKey": "category",
     "header": "Category",
@@ -155,14 +161,19 @@ const headers = [
 
 const CashFlowTable = () => {
 
-  const [visibleColumns, setVisibleColumns] = useState(['category', '2024-03', '2024-04', '2024-05', 'TTM']);
+  // const [visibleColumns, setVisibleColumns] = useState(['category', '2024-03', '2024-04', '2024-05', 'TTM']);
+
+  const [data, setData] = useState(res['rows']);
 
   // const columns = useMemo(() => headers, []);
   // .filter(col => visibleColumns.includes(col.accessorKey))
   const columns = useMemo(() => {
-    return headers.map(col => ({
+    return res['columns'].map(col => ({
       ...col,
       header: !(['TTM', 'Category', ''].includes(col.header)) ? formatDateHeader(col.header) : col.header,
+      muiTableBodyCellProps: {
+        align: (['Category'].includes(col.header)) ? 'left' : 'right'
+      },
     }));
   }, []);
 
@@ -176,7 +187,7 @@ const CashFlowTable = () => {
     enableFilters: false,
     enableColumnActions: false,
     enablePagination: false,
-    layoutMode: 'fixed',
+    // layoutMode: 'fixed',
     initialState: {
       density: 'compact'
     },
@@ -189,7 +200,7 @@ const CashFlowTable = () => {
           sx: () => ({
             maxWidth: '4rem',
             width: '4rem',
-            backgroundColor: (['Net Income', 'Income', 'Expenses'].includes(row.original.category)) ? '#F5F5F5' : ''
+            backgroundColor: (['Net Profit', 'Income', 'Expenses'].includes(row.original.category)) ? '#F5F5F5' : ''
           }),
         }),
         size: 40,
@@ -198,10 +209,12 @@ const CashFlowTable = () => {
     },
     muiTableBodyCellProps: ({ row }) => ({
       sx: {
-        backgroundColor: (['Net Income', 'Income', 'Expenses'].includes(row.original.category)) ? '#F5F5F5' : ''
+        backgroundColor: (['Net Profit', 'Income', 'Expenses'].includes(row.original.category)) ? '#F5F5F5' : ''
       },
     }),
-
+    muiTableHeadCellProps: ({ column }) => ({
+      align: (['category'].includes(column.id)) ? 'center' : 'right',
+    }),
   });
 
   return <MaterialReactTable table={table} />;
