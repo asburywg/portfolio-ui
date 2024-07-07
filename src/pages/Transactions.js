b2b4a6a269ab16b360315b2adab37384c256d1fa
Will Asbury <asburywg@gmail.com>
2024-07-06 22:22:46 -0400

    filters

--- src/pages/Transactions.js
+++ src/pages/Transactions.js
@@ -6,19 +6,23 @@ export default function Transactions() {
 
     return (
         <TransactionProvider>
-            <TabView>
-                <TabPanel header="Mint">
-                    <div className="h-5/6 w-11/12 mx-auto mt-0">
+            <div className="flex flex-col h-screen">
+                <div className="flex-1 overflow-hidden">
+                    <div className="h-5/6 w-11/12 mx-auto mt-5">
                         <TransactionsTable filterable />
                     </div>
-
-                </TabPanel>
-                {/* <TabPanel header="Rollup Explorer"> */}
-                    {/* <div className="h-3/4 w-4/5 mx-auto mt-2"> */}
-                        {/* <TransactionsTable filterable /> */}
-                    {/* </div> */}
-                {/* </TabPanel> */}
-            </TabView>
+                </div>
+            </div>
         </TransactionProvider>
+        // <TabView>
+        //     <TabPanel header="Mint">
+        //     </TabPanel>
+        //     {/* <TabPanel header="Rollup Explorer"> */}
+        //         {/* <div className="h-3/4 w-4/5 mx-auto mt-2"> */}
+        //             {/* <TransactionsTable filterable /> */}
+        //         {/* </div> */}
+        //     {/* </TabPanel> */}
+        // </TabView>
+
     )
 }
